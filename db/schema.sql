4fc6dfdf9128250d648c715a30e0fe77a18ff15c
Beth Stevic <bethstevic89@gmail.com>
2017-05-25 14:46:21 -0700

    update schema for planed implementation(for phase1)

--- db/schema.sql
+++ db/schema.sql
@@ -5,76 +5,78 @@ USE tiny_task;
 --hold users and auth0 tokens
 CREATE TABLE IF NOT EXISTS users (
   auth_token TEXT NOT NULL,
-  username TEXT NOT NULL,
+  user_name TEXT NOT NULL,
+  user_status TEXT,
   PRIMARY KEY (auth_token)
 );
 
---holds teams and a bulletin board
+--holds team & project name
 CREATE TABLE IF NOT EXISTS teams (
   id INTEGER NOT NULL AUTO_INCREMENT,
-  bulletin_board TEXT,
+  project_name TEXT,
   PRIMARY KEY (id)
 );
 
 --join table to match teams up with members
-CREATE TABLE IF NOT EXISTS team_members (
+CREATE TABLE IF NOT EXISTS team_users (
   team_id INTEGER NOT NULL,
-  user_id INTEGER NOT NULL,
+  user_id TEXT NOT NULL,
   FOREIGN KEY (team_id) REFERENCES teams(id),
-  FOREIGN KEY (member_id) REFERENCES users(auth_token)
+  FOREIGN KEY (user_id) REFERENCES users(auth_token)
+);
+
+--holds announcements for bulletin board
+CREATE TABLE IF NOT EXISTS announcements (
+  id INTEGER NOT NULL AUTO_INCREMENT,
+  announcement TEXT NOT NULL,
+  user_id TEXT NOT NULL,
+  team_id INTEGER NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (user_id) REFERENCES users(auth_token),
+  FOREIGN KEY (team_id) REFERENCES teams(id)
 );
 
 --holds messages, user ids and team ids
 CREATE TABLE IF NOT EXISTS messages (
   id INTEGER NOT NULL AUTO_INCREMENT,
   message TEXT NOT NULL,
-  user_id INTEGER NOT NULL,
+  user_id TEXT NOT NULL,
   team_id INTEGER NOT NULL,
   PRIMARY KEY (id),
   FOREIGN KEY (team_id) REFERENCES teams(id),
   FOREIGN KEY (user_id) REFERENCES users(auth_token)
 );
 
---holds project
-CREATE TABLE IF NOT EXISTS project (
-  id INTEGER NOT NULL AUTO_INCREMENT,
-  project_name TEXT NOT NULL,
-);
 
 --holds phases of projects ex phase 1, phase 2
 CREATE TABLE IF NOT EXISTS phases (
   id INTEGER NOT NULL AUTO_INCREMENT,
-  phases_name TEXT NOT NULL,
-  project_id INTEGER NOT NULL,
+  phase_name TEXT NOT NULL,
+  team_id INTEGER NOT NULL,
+  phase_status TEXT,
   PRIMARY KEY (id),
-  FOREIGN KEY (project_id) REFERENCES project(id),
-  FOREIGN KEY (task_id) REFERENCES phases(id)
+  FOREIGN KEY (team_id) REFERENCES teams(id),
 );
 
---holds task that pertain to a phase. One task can have one phases, a phase can have multiple tasks
+--holds task that pertain to a phase. One task can have one phase, a phase can have multiple tasks
 CREATE TABLE IF NOT EXISTS tasks (
   id INTEGER NOT NULL AUTO_INCREMENT,
   task_name TEXT NOT NULL,
-  phase_id TEXT,
+  task_status TEXT,
+  phase_id INTEGER NOT NULL,
   PRIMARY KEY (id),
   FOREIGN KEY (phase_id) REFERENCES phases(id)
 );
 
 --join table for users and the tasks they are assigned. One task can have multiple users, one user can have multiple tasks
 CREATE TABLE IF NOT EXISTS users_tasks (
-  user_id INTEGER NOT NULL,
+  user_id TEXT NOT NULL,
   task_id INTEGER NOT NULL,
   FOREIGN KEY (user_id) REFERENCES users(auth_token),
   FOREIGN KEY (task_id) REFERENCES tasks(id)
 );
 
---join table for users and projects. One user can have multiple projects and one project can have multiple users
-CREATE TABLE IF NOT EXISTS users_projects (
-  user_id INTEGER NOT NULL,
-  team_id INTEGER NOT NULL,
-  FOREIGN KEY (user_id) REFERENCES users(auth_token),
-  FOREIGN KEY (team_id) REFERENCES teams(id)
-);
+
 
 
 
