8afb34f0d09e357c0ae803d261d00cfef18b70e6
Beth Stevic <bethstevic89@gmail.com>
2017-05-25 16:09:53 -0700

    Set up routers and requestHandler functions (for Phase 1)

---
+++ util/helperFunctions.js
@@ -0,0 +1,50 @@
+const mysql = require('mysql');
+
+const connection = sequelize.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'tiny'
+});
+
+exports.addUser
+
+exports.updateUser
+
+exports.deleteUser
+
+exports.addTeam
+
+exports.updateTeam
+
+exports.deleteTeam
+
+exports.addMessage
+
+exports.deleteMessage
+
+exports.addAnnouncement
+
+exports.deleteAnnouncement
+
+exports.addProject
+
+exports.updateProject
+
+exports.deleteProject
+
+exports.addPhase
+
+exports.updatePhase
+
+exports.deletePhase
+
+exports.addTask
+
+exports.updateTask
+
+exports.deleteTask
+
+exports.newResource
+
+exports.deleteResource
--- util/requestHandler.js
+++ util/requestHandler.js
@@ -1,2 +1,13 @@
-const db = require('../db/schema.js');
+const db = require('../db/models.js');
+
+module.exports = {
+  users: {GET, POST, PUT, DELETE},
+  teams: {GET, POST, PUT, DELETE},
+  messages: {GET, POST, PUT, DELETE},
+  anouncements: {GET, POST, PUT, DELETE},
+  projects: {GET, POST, PUT, DELETE},
+  phases: {GET, POST, PUT, DELETE},
+  tasks: {GET, POST, PUT, DELETE},
+  resources: {GET, POST, PUT, DELETE}
+}
 
