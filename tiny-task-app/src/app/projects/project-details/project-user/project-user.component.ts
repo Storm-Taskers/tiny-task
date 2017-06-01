efc630e07f205eee68433b92f534d2b774397736
Kevin Nguyen <knguy47@gmail.com>
2017-06-01 01:35:30 -0500

    Fix selector for User-details and add HTML and CSS to project-user

--- tiny-task-app/src/app/projects/project-details/project-user/project-user.component.ts
+++ tiny-task-app/src/app/projects/project-details/project-user/project-user.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-user',
+  selector: 'user',
   templateUrl: './project-user.component.html',
   styleUrls: ['./project-user.component.css']
 })
