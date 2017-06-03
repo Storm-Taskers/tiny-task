56a277c308453bc7bc9f78e88549a2d672bb9821
David Hsiao <dhsiao89@gmail.com>
2017-06-03 00:18:06 -0700

    Add addPhases helper/handlers

--- routes.js
+++ routes.js
@@ -1,58 +1,51 @@
-const router = require('express').Router();
-const handler = require('./util/requestHandler.js');
-
+const router = require("express").Router();
+const handler = require("./util/requestHandler.js");
 
 // router.get('/login', -)
 
 //////////USERS/////////////////
-router.get('/users/:auth_token', handler.users.retrieveUser);
-router.post('/users/', handler.users.createNewUser);
-router.put('/users/:user_id', handler.users.updateUser);
-router.delete('/users/:user_id', handler.users.deleteUser);
+router.get("/users/:auth_token", handler.users.retrieveUser);
+router.post("/users/", handler.users.createNewUser);
+router.put("/users/:user_id", handler.users.updateUser);
+router.delete("/users/:user_id", handler.users.deleteUser);
 
 //////////TEAMS/////////////////
-router.get('/teams/:team_id', handler.teams.retrieveTeams);
+router.get("/teams/:team_id", handler.teams.retrieveTeams);
 router.get("/teams/users/:user_id", handler.users.getUserTeams);
-router.post('/teams', handler.teams.createNewTeams);
-router.put('/teams/:team_id', handler.teams.updateTeams);
-router.delete('/teams/:team_id', handler.teams.deleteTeams);
-router.delete('/teams/users/:user_id/:team_id', handler.users.deleteTeamUsers);
+router.post("/teams", handler.teams.createNewTeams);
+router.put("/teams/:team_id", handler.teams.updateTeams);
+router.delete("/teams/:team_id", handler.teams.deleteTeams);
+router.delete("/teams/users/:user_id/:team_id", handler.users.deleteTeamUsers);
 
 //////////PROJECTS/////////////////
-router.get('/projects/:project_id', handler.projects.retrieveProjectById);
-router.post('/projects', handler.projects.createNewProjects);
-router.put('/projects/:project_id', handler.projects.updateProjects);
-router.delete('/projects/:project_id', handler.projects.deleteProjects);
-
+router.get("/projects/:project_id", handler.projects.retrieveProjectById);
+router.post("/projects", handler.projects.createNewProjects);
+router.put("/projects/:project_id", handler.projects.updateProjects);
+router.delete("/projects/:project_id", handler.projects.deleteProjects);
 
 //////////PHASES/////////////////
-router.post('/phases/:project_id', handler.phases.createNewPhases);
-router.put('/phases/:phase_id', handler.phases.updatePhases);
-router.delete('/phases/:phase_id', handler.phases.deletePhases);
-
+router.post("/phases/:project_id", handler.phases.createNewPhases);
+router.put("/phases/:phase_id", handler.phases.updatePhases);
+router.delete("/phases/:phase_id", handler.phases.deletePhases);
 
 //////////TASKS/////////////////
-router.get('/tasks/:phase_id', handler.tasks.retrieveTasksByPhaseId);
+router.get("/tasks/:phase_id", handler.tasks.retrieveTasksByPhaseId);
 //router.get('/tasks/users/:user_id', handler.users.getUserTasks);
-router.post('/tasks/:phase_id', handler.tasks.createNewTasks);
-router.put('/tasks/:task_id', handler.tasks.updateTasks);
-router.delete('/tasks/:task_id', handler.tasks.deleteTasks);
-router.delete('/tasks/users/:user_id/:task_id', handler.users.deleteTaskUsers);
-
-
+router.post("/tasks/:phase_id", handler.tasks.createNewTasks);
+router.put("/tasks/:task_id", handler.tasks.updateTasks);
+router.delete("/tasks/:task_id", handler.tasks.deleteTasks);
+router.delete("/tasks/users/:user_id/:task_id", handler.users.deleteTaskUsers);
 
 //////////MESSAGES/////////////////
 // router.get('/api/messages', handler.messages.retrieveMessages);
 // router.post('/api/messages', handler.messages.createNewMessages);
 // router.delete('/api/messages', handler.messages.deleteMessages);
 
-
 //////////ANNOUNCEMENTS/////////////////
 // router.get('/api/announcements', handler.announcements.retrieveAnnouncements);
 // router.post('/api/announcements', handler.announcements.createNewAnnouncements);
 // router.put('/api/announcements', handler.announcements.updateAnnouncements);
 // router.delete('/api/announcements', handler.announcements.deleteAnnouncements);
-
 
 //////////RESOURCES/////////////////
 // router.get('/resources', handler.resources.retrieveResources);
