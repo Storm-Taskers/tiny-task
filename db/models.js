d0bf3cb69ba7ba008bed1c9cf83ca2b871662cc1
Beth Stevic <bethstevic89@gmail.com>
2017-06-10 17:40:28 -0700

    Modify db for Cleardb (Phase 3)

--- db/models.js
+++ db/models.js
@@ -7,7 +7,7 @@ const Sequelize = require("sequelize");
 //host: "us-cdbr-east.cleardb.com" || "localhost",
 //"heroku_117a59f899c5677" || "tiny_task", "bc4f4387ccab9d" || "root", "74c98b07" || ""
 
-const connection = new Sequelize("mysql://bc4f4387ccab9d:74c98b07@us-cdbr-east.cleardb.com/heroku_db?reconnect=true", {
+const connection = new Sequelize("mysql://bc4f4387ccab9d:74c98b07@us-cdbr-east.cleardb.com/heroku_117a59f899c5677?reconnect=true", {
           dialect: "mysql"
         } );
 
