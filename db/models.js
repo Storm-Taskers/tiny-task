e512e3a752421060f49d9b28e117a1d683069fc1
Beth Stevic <bethstevic89@gmail.com>
2017-06-12 11:06:12 -0700

    Modify db for postgres (Phase 3)

--- db/models.js
+++ db/models.js
@@ -31,8 +31,6 @@ let connection;
     connection = new Sequelize(process.env.DATABASE_URL, {
       dialect:  'postgres',
       protocol: 'postgres',
-      port:     match[4],
-      host:     match[3],
       logging:  false
     });
   } else {
