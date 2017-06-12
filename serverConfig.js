550c0d111d7e71d8c5a303638c8ffa512fa8a155
Beth Stevic <bethstevic89@gmail.com>
2017-06-12 11:29:13 -0700

    Modify serverConfig for heroku (Phase 3)

--- serverConfig.js
+++ serverConfig.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const router = require('./routes.js');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 
@@ -10,5 +11,9 @@ app.use(cors());
 app.use('/api', router);
 
 app.use(express.static(__dirname + '/tiny-task-app/dist'));
+
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/tiny-task-app/dist/index.html'));
+});
 
 module.exports = app;
