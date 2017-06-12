db0f567db17c9262bbeef118de50727fe2f539e9
Beth Stevic <bethstevic89@gmail.com>
2017-06-12 11:40:07 -0700

    Modify serverConfig for heroku (Phase 3)

--- serverConfig.js
+++ serverConfig.js
@@ -13,7 +13,7 @@ app.use('/api', router);
 app.use(express.static(__dirname + '/tiny-task-app/dist'));
 
 app.get('/*', function(req, res) {
-  res.sendFile(__dirname + '/tiny-task-app/dist/index.html');
+  res.sendFile('/tiny-task-app/dist/index.html');
 });
 
 module.exports = app;
