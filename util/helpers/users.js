3ff86ce592da0ae8a07a3a189a1ec56c7b3de27c
Brian Leung <brianbrianleungleung@gmail.com>
2017-06-14 03:42:23 -0500

    Add routes and handler functions for retrieving user profile
    Create API endpoint for user profile retrieval.
    Add request handler and helper functions to fetch user profile from database.
    Implement getUserProfile function in frontend within User service.

--- util/helpers/users.js
+++ util/helpers/users.js
@@ -21,6 +21,16 @@ exports.retrieveUser = (authToken, callback) => {
   });
 };
 
+exports.retrieveProfile = (userId, callback) => {
+  models.User_Profile.findOne({
+    where: {
+      id: userId
+    }
+  }).then(user_profile => {
+    callback(user_profile);
+  });
+};
+
 exports.addUsers = (body, id, callback) => {
   models.Users.create({
       auth_token: body.auth_token,
