06cf345eceb93395bc3c1cd5df7dfddc390a6c8b
Beth Stevic <bethstevic89@gmail.com>
2017-06-14 14:21:12 -0700

    Fix merge conflicts consolidating package jsons (Phase 3)

---
+++ src/app/services/shared-resources-service/shared-resources.service.ts
@@ -0,0 +1,73 @@
+import { Headers, Http } from '@angular/http';
+import { Injectable } from '@angular/core';
+
+// Import ReactiveJS toPromise
+import 'rxjs/add/operator/toPromise';
+
+import { Resource } from '../../shared-resources/shared-resources';
+import { User } from '../../projects/project-details/project-user/User';
+
+@Injectable()
+export class SharedResourceService {
+  private headers = new Headers({'Content-type': 'application/JSON'});
+  private baseUrl: string = 'http://localhost:8080';
+
+  public resources: Resource[] = [];
+  public resourceUser: Array<User> = [];
+
+
+  constructor(private http: Http) { }
+
+  private handleError(error: any): Promise<any> {
+    console.error('An error occured', error);
+    return Promise.reject(error.message || error);
+  }
+
+  // Fetch Information
+  getResources(teamId: number): void {
+    this.http.get(`${this.baseUrl}/api/resources/${teamId}`)
+      .toPromise()
+      .then( (response) => {
+        this.resources = response.json();
+      })
+      .catch(this.handleError);
+  }
+
+  // Post Information
+  createResource(teamId: number, userId: number, resource: string, resourceNote: string): void {
+    this.http.post(
+      `${this.baseUrl}/api/resources`,
+      JSON.stringify({resource: resource, resourceNote: resourceNote, user_id: userId, team_id: teamId}),
+      {headers: this.headers})
+      .toPromise()
+      .then( (response) => {
+        this.resources.push(response.json().resource);
+        this.resourceUser.push(response.json().user_id);
+      })
+      .catch(this.handleError);
+  }
+
+  // Edit Information
+  editResource(resourceId: number, resource: string, resourceNote: string): void {
+    this.http.put(
+      `${this.baseUrl}/api/resources/${resourceId}`,
+      JSON.stringify({resourceId: resourceId, resourceChanges: {resource: resource, resourseNote: resourceNote}}),
+      {headers: this.headers})
+      .toPromise()
+      .then( (response) => {
+        this.resources.find(resource => resourceId === resourceId)
+      })
+      .catch(this.handleError);
+  }
+
+  // Delete Information
+  deleteResource(resourceId: number): void {
+    this.http.delete(`${this.baseUrl}/api/resources/${resourceId}`)
+      .toPromise()
+      .then( (response) => {
+        let resourceToRemove = this.resources.findIndex(resource => resource.id === resourceId);
+        this.resources.splice(resourceToRemove, 1);
+      })
+      .catch(this.handleError);
+  }
+}
