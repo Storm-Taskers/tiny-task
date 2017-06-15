2a37caf6cfcc7a8b96ace11aa08510e342031038
Kevin Nguyen <knguy47@gmail.com>
2017-06-14 21:30:24 -0500

    Add env variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,9 @@
 export const environment = {
   production: true,
+<<<<<<< 87807b67d6e1e2704d89a005dcc1fee738f6dba9
   serverUrl: 'https://tinytask.herokuapp.com',
   appUrl: 'http://tinytask.herokuapp.com'
+=======
+  url: 'https://http://tinytask.herokuapp.com/'
+>>>>>>> Add env variables
 };
